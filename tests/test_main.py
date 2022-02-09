db1d9c1027b44ed8dbfa5610f3fe3c5091c96df3
Adam Podhorski <apodhorski@unav.es>
2022-02-09 11:25:13 +0100

    Add function added.

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,17 @@ def test_add_two_numbers():
     
     assert result == 5
     
+def test_add_three_numbers():
+    calculator = Calc() # Instantation
     
+    result = calculator.add(2, 3, 4)
+    
+    assert result == 9    
+
+def test_add_many_numbers():
+    numbers = range(100)
+    calculator = Calc() # Instantation
+    
+    result = calculator.add(*numbers)
+    
+    assert result == 4950  
